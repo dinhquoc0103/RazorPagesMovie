1b162311a70e01a9e8f486ab53b78063da9143e3
TienQuach <tienquach@sorimachigroup.vn>
2021-04-08 17:19:14 +0700

    tao razor page movie

---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        // ID phim
+        public int ID { get; set; }
+        //Tên phim 
+        public string Title { get; set; }
+
+        // Ngày phát hành
+        [DataType(DataType.Date)]   // Validate là ngày
+        public DateTime ReleaseDate { get; set; }
+        // Thể loại phim
+        public string Genre { get; set; }
+        //Giá
+        public decimal Price { get; set; }
+    }
+}
